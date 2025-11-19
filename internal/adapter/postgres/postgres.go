ed369d80a66b9a7c6981d8a6d8e796235a8733d8
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-11-20 00:12:14 +0100

    feat: add connection pool for psql

--- internal/adapter/postgres/postgres.go
+++ internal/adapter/postgres/postgres.go
@@ -1,14 +1,19 @@
 package postgres
 
 import (
+	"time"
+
 	"github.com/eerzho/telegram-ai/pkg/lru"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq" // PostgreSQL driver
 )
 
 type Config struct {
-	URL           string `env:"POSTGRES_URL,required"`
-	STMTCacheSize int    `env:"POSTGRES_STMT_CACHE_SIZE" envDefault:"10"`
+	URL             string        `env:"POSTGRES_URL,required"`
+	STMTCacheSize   int           `env:"POSTGRES_STMT_CACHE_SIZE" envDefault:"10"`
+	MaxOpenConns    int           `env:"POSTGRES_MAX_OPEN_CONNS"    envDefault:"25"`
+	MaxIdleConns    int           `env:"POSTGRES_MAX_IDLE_CONNS"    envDefault:"5"`
+	ConnMaxLifetime time.Duration `env:"POSTGRES_CONN_MAX_LIFETIME" envDefault:"5m"`
 }
 
 type DB struct {
@@ -21,6 +26,11 @@ func New(cfg Config) *DB {
 	if err != nil {
 		panic(err)
 	}
+
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	db.SetConnMaxLifetime(cfg.ConnMaxLifetime)
+
 	return &DB{
 		db:        db,
 		stmtCache: lru.NewCache[*sqlx.Stmt](cfg.STMTCacheSize),
