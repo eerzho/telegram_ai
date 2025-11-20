594aeea8a0e5371e96317da128ea5214a97ab859
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-11-20 15:19:53 +0100

    fix: add content type validation in decode and remove panic

--- pkg/json/json.go
+++ pkg/json/json.go
@@ -2,8 +2,14 @@ package json
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
+	"mime"
 	"net/http"
+)
+
+var (
+	ErrInvalidContentType = errors.New("Content-Type must be application/json")
 )
 
 type ErrorResponse struct {
@@ -12,6 +18,21 @@ type ErrorResponse struct {
 
 func Decode[T any](r *http.Request) (T, error) {
 	var v T
+
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "" {
+		return v, fmt.Errorf("json decode: %w", ErrInvalidContentType)
+	}
+
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		return v, fmt.Errorf("json decode: %w", err)
+	}
+	
+	if mediaType != "application/json" {
+		return v, fmt.Errorf("json decode: %w", ErrInvalidContentType)
+	}
+
 	if err := json.NewDecoder(r.Body).Decode(&v); err != nil {
 		return v, fmt.Errorf("json decode: %w", err)
 	}
@@ -23,7 +44,6 @@ func Encode[T any](w http.ResponseWriter, r *http.Request, status int, v T) {
 	w.WriteHeader(status)
 	if err := json.NewEncoder(w).Encode(v); err != nil {
 		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		panic(err)
 	}
 }
 
