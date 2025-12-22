a8cc2f19a80d07b108652f3c41ae27a5c98c634e
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-12-22 16:11:07 +0100

    fix: add stop event for err chan and fix text in stop with error event

--- internal/summary/summary_generate/dto.go
+++ internal/summary/summary_generate/dto.go
@@ -60,9 +60,12 @@ func (o *Output) Next() (sse.Event, bool) {
 			return sse.Event{Name: "stop"}, true
 		}
 		return sse.Event{Name: "append", Data: text}, false
-	case err := <-o.ErrChan:
+	case err, ok := <-o.ErrChan:
+		if !ok {
+			return sse.Event{Name: "stop"}, true
+		}
 		if err != nil {
-			return sse.Event{Name: "stop_with_error", Data: "Please try again later."}, true
+			return sse.Event{Name: "stop_with_error", Data: "{ \"text\": \"Please try again later.\" }"}, true
 		}
 		return sse.Event{}, true
 	}
