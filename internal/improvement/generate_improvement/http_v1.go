832b08d867ee9b7e9c442bc35389b41d317c83ea
Zhanbolat <z.yerkinbay@slotegrator.space>
2025-12-25 01:05:50 +0100

    fix: change pkg name

---
+++ internal/improvement/generate_improvement/http_v1.go
@@ -0,0 +1,50 @@
+package generateimprovement
+
+import (
+	"log/slog"
+	"net/http"
+
+	errorhelp "github.com/eerzho/goiler/pkg/error_help"
+	httpjson "github.com/eerzho/goiler/pkg/http_json"
+	httpserver "github.com/eerzho/goiler/pkg/http_server"
+	"github.com/eerzho/telegram_ai/pkg/sse"
+)
+
+// HTTPv1 godoc
+//
+// @summary improvement generate
+// @tags improvement
+//
+// @accept json
+// @param request body Input true "body"
+//
+// @produce json,event-stream
+// @success 200 {object} sse.Event
+// @failure 400 {object} httpjson.Error
+// @failure 500 {object} httpjson.Error
+//
+// @router /v1/improvements/generate [post].
+func HTTPv1(logger *slog.Logger, usecase *Usecase) httpserver.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) error {
+		const op = "improvement_generate.HTTPv1"
+
+		ctx := r.Context()
+		defer r.Body.Close()
+
+		input, err := httpjson.Decode[Input](r)
+		if err != nil {
+			return errorhelp.WithOP(op, err)
+		}
+
+		output, err := usecase.Execute(ctx, input)
+		if err != nil {
+			return errorhelp.WithOP(op, err)
+		}
+
+		if err = sse.Stream(ctx, w, &output); err != nil {
+			logger.ErrorContext(ctx, "stream error", slog.Any("error", errorhelp.WithOP(op, err)))
+		}
+
+		return nil
+	}
+}
