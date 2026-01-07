202ebbab22243dbef59de0fcffe81e5036a4cc6f
Zhanbolat <z.yerkinbay@slotegrator.space>
2026-01-07 17:11:20 +0100

    fix: change domain response

--- internal/domain/response.go
+++ internal/domain/response.go
@@ -1,24 +1,23 @@
 package domain
 
-type ResponseType int
+type ResponseType string
 
 const (
-	ResponseTypeMessage ResponseType = iota
-	ResponseTypeReaction
-	ResponseTypeSkip
+	ResponseTypeMessage  ResponseType = "message"
+	ResponseTypeReaction ResponseType = "reaction"
+	ResponseTypeSkip     ResponseType = "skip"
 )
 
-type ReactionType int
+type ReactionType string
 
 const (
-	ReactionTypeZeroVal ReactionType = iota
-	ReactionTypeLike
-	ReactionTypeOK
-	ReactionTypeNice
+	ReactionTypeLike ReactionType = "like"
+	ReactionTypeOK   ReactionType = "ok"
+	ReactionTypeNice ReactionType = "nice"
 )
 
 type Response struct {
 	Type         ResponseType `json:"type"`
 	Message      string       `json:"message"`
-	ReactionType ReactionType `json:"reaction_type"`
+	ReactionType ReactionType `json:"reaction_type,omitempty"`
 }
